952fe49e61a4fe32a482a268b45dd5c74ba9fbd8
Syreldar <enrico.drago3@gmail.com>
2023-09-22 18:46:20 +0200

    ups.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,21 +10,21 @@ public class Main
     {
         var person = new Person("Enrico", 10);
         var cutleryMap = Map.of(
-        "fork", new Fork(15),
-        "knife", new Knife(10),
-        "spoon", new Spoon(5)
+            "fork", new Fork(15),
+            "knife", new Knife(10),
+            "spoon", new Spoon(5)
         );
 
         var menu = Map.of(
-        "first_course", new Pasta(50),
-        "fruit", new Apple(20),
-        "snack", new Cake(30)
+            "first_course", new Pasta(50),
+            "fruit", new Apple(20),
+            "snack", new Cake(30)
         );
 
         var eatingUtensils = Map.of(
-        "first_course", "fork",
-        "fruit", "knife",
-        "snack", "spoon"
+            "first_course", "fork",
+            "fruit", "knife",
+            "snack", "spoon"
         );
 
         eatingUtensils.forEach((foodKey, utensilKey) ->
